6c65af0c564124fe86871c5746e24c6651b24e0f
nikozdev <nikozdev@nikozdev.net>
2023-08-23 02:36:59 -0700

    !code!main!envi!test!glfw!libs!deps!success!update!extend
    [code/Main.cpp]
    > implemented glfw test
    > fixed testing
    [envi/CMakeLists.txt]
    > fixed tests
    > fixed compile_commands
    > using path variables

--- fork/code/Main.cpp
+++ fork/code/Main.cpp
@@ -2,18 +2,20 @@
 #include <string_view>
 #include <functional>
 #include <unordered_map>
-
+#include <GLFW/glfw3.h>
+//forward
+typedef struct GLFWwindow GLFWwindow;
+//typedef
 using tTestKey = std::string_view;
 using tTestFun = std::function<int(void)>;
 using tTestTab = const std::unordered_map<tTestKey, tTestFun>;
 using tTestRef = tTestTab::const_iterator;
-
-extern tTestTab vTestTab;
-
+//actions
 int main(int vArgC, char **vArgV)
 {
-  if (vArgC == 2 ? (std::string_view(vArgV[1]) == "test") : 0)
+  if (vArgC == 3 ? (std::string_view(vArgV[1]) == "test") : 0)
   {
+    extern tTestTab vTestTab;
     tTestRef vTestRef = vTestTab.find(vArgV[2]);
     if (vTestRef == vTestTab.end())
     {
@@ -37,7 +39,27 @@ int main(int vArgC, char **vArgV)
   }
   return EXIT_SUCCESS;
 }//main
-
+using tKeyFun = std::function<void(GLFWwindow*vWindow, int vActn, int vMods)>;
+using tKeyMap = std::unordered_map<int, tKeyFun>;
+using tKeyRef = tKeyMap::iterator;
+tKeyMap vKeyMap =
+{
+  {
+    GLFW_KEY_Q,
+    [](GLFWwindow* vWindow, int vActn, int vMods)
+    {
+      glfwSetWindowShouldClose(vWindow, 1);
+    }
+  },
+};
+void fKeyProc(GLFWwindow* vWindow, int vCode, int _, int vActn, int vMods)
+{
+  auto vKeyRef = vKeyMap;
+  if (tKeyRef vKeyRef = vKeyMap.find(vCode); vKeyRef != vKeyMap.end())
+  {
+    vKeyRef->second(vWindow, vActn, vMods);
+  }
+}//fKeyProc
 tTestTab vTestTab = {
   { "HelloWorld",
     []()
@@ -46,14 +68,29 @@ tTestTab vTestTab = {
       return EXIT_SUCCESS;
     }
   },//HelloWorld
-  { "GlfwWindowCreate",
+  { "GlfwWindow",
     []()
     {
-      if (glfwInit())
+      if (glfwInit() == 0)
       {
+        std::cerr << "failed glfw init";
+        return EXIT_FAILURE;
       }
-      std::cout << "GlfwWindowCreate_Success" << std::endl;
+      GLFWwindow*vWindow = glfwCreateWindow(0xff,0xff, "GlfwWindow", NULL,NULL);
+      if (vWindow == NULL)
+      {
+        std::cerr << "failed window creation";
+        return EXIT_FAILURE;
+      }
+      glfwShowWindow(vWindow);
+      glfwSetKeyCallback(vWindow, fKeyProc);
+      for(int vFlag = 1; vFlag; vFlag = !glfwWindowShouldClose(vWindow))
+      {
+        glfwSwapBuffers(vWindow);
+        glfwPollEvents();
+      }
+      std::cout << "GlfwWindow_Success" << std::endl;
       return EXIT_SUCCESS;
     }
   },
-};
+};//vTestTab
